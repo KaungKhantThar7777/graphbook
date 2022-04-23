b53a3f4fb2401e1d28e777575182fd5171a63f1b
kaungkhantthar99 <kaungkhantthar@rezeve.com>
2022-04-23 12:32:12 +0630

    setup graphql server

--- src/server/index.js
+++ src/server/index.js
@@ -6,32 +6,51 @@ import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
 
+// services
+import services from "./services";
+
 const root = path.join(__dirname, "../../");
 
 const app = express();
 
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "*.amazonaws.com"],
-    },
-  })
-);
-app.use(
-  helmet.referrerPolicy({
-    policy: "same-origin",
-  })
-);
+if (process.env.NODE_ENV === "production") {
+  console.log("run");
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"],
+      },
+    })
+  );
+  app.use(
+    helmet.referrerPolicy({
+      policy: "same-origin",
+    })
+  );
+}
 
 app.use(compression());
 app.use(cors());
 
 app.use("/", express.static(path.join(root, "/dist/client")));
 app.use("/uploads", express.static(path.join(root, "/uploads")));
+
+const serviceNames = Object.keys(services);
+
+for (let name of serviceNames) {
+  if (name === "graphql") {
+    (async () => {
+      await services[name].start();
+      services[name].applyMiddleware({ app });
+    })();
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(root, "/dist/client/index.html"))
---
+++ src/server/services/graphql/index.js
@@ -0,0 +1,17 @@
+import { ApolloServer } from "apollo-server-express";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+import Resolvers from "./resolvers";
+import Schema from "./schema";
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers,
+});
+
+const server = new ApolloServer({
+  schema: executableSchema,
+  context: ({ req }) => req,
+});
+
+export default server;
